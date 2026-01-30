b68c24a680a2bc89590b53d269e93cdae84a4340
MushyKeycaps <abhi.soy.332@gmail.com>
2026-01-31 05:22:32 +0530

    logo changes

--- backend/index.js
+++ backend/index.js
@@ -1,41 +1,43 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import 'dotenv/config'
+import "dotenv/config";
 
-import connectDB from "./db.js"
+import connectDB from "./db.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-import authRoutes from './src/routes/auth.js'
-import userRoutes from './src/routes/userRoutes.js'
-import sessionRoutes from './src/routes/sessionRoutes.js'
+import authRoutes from "./src/routes/auth.js";
+import userRoutes from "./src/routes/userRoutes.js";
+import sessionRoutes from "./src/routes/sessionRoutes.js";
 
-app.use(cors({ 
-                origin: 'http://localhost:5173',
-                credentials: true
-            }));
+app.use(
+    cors({
+        origin: "http://localhost:4173",
+        credentials: true,
+    }),
+);
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authRoutes)
-app.use('/api/users', userRoutes)
-app.use('/api/sessions', sessionRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/sessions", sessionRoutes);
 
-app.get('/', (req,res) => {
-    res.json({message: "Backend is online."})
-})
+app.get("/", (req, res) => {
+    res.json({ message: "Backend is online." });
+});
 
 const startServer = async () => {
-    try{
+    try {
         await connectDB();
         app.listen(PORT, () => {
-            console.log('App runnin on port', PORT)
-        })
+            console.log("App runnin on port", PORT);
+        });
     } catch (err) {
-        console.error(err)
+        console.error(err);
     }
-}
+};
 
 startServer();
