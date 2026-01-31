9a77227649ee1c8f6bc5a9a7fe383eaa152be6e5
MushyKeycaps <abhi.soy.332@gmail.com>
2026-01-31 05:56:56 +0530

    fixed login form dismounting issue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,96 +1,126 @@
-
-import './App.css'
-import SessionDetails from './components/SessionDetails'
-import CreateSession from './components/CreateSession'
-import { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { ThemeModal } from './components/ThemeModal'
-import SessionCategories from './components/SessionCategories'
-import Background from './components/Background'
-import TopSection from './components/TopSection'
-import PreviousSessions from './components/PreviousSessions'
-import { authCheck } from './state/user/userThunks'
-import { all, recent } from './state/session/sessionThunks'
-import LoginForm from './components/LoginForm'
+import './App.css';
+import SessionDetails from './components/SessionDetails';
+import CreateSession from './components/CreateSession';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { ThemeModal } from './components/ThemeModal';
+import SessionCategories from './components/SessionCategories';
+import Background from './components/Background';
+import TopSection from './components/TopSection';
+import PreviousSessions from './components/PreviousSessions';
+import { authCheck } from './state/user/userThunks';
+import { all, recent } from './state/session/sessionThunks';
+import LoginForm from './components/LoginForm';
 
 function App() {
-  const dispatch = useDispatch();
-  const [create, setCreate] = useState(true)
-  const { theme, modalOpen } = useSelector( s => s.ui )
-  const { recentSessions } = useSelector( s => s.session )
-  const { profile, status, error, isAuthenticated } = useSelector( s => s.user )
+    const dispatch = useDispatch();
+    const [create, setCreate] = useState(true);
+    const { theme, modalOpen } = useSelector((s) => s.ui);
+    const { recentSessions } = useSelector((s) => s.session);
+    const { profile, status, error, isAuthenticated } = useSelector(
+        (s) => s.user
+    );
 
-  useEffect(() => {
-      dispatch(authCheck());
-      if(isAuthenticated){
-
-        dispatch(all());
-        dispatch(recent());
-      }
+    const isBooting = status === 'idle' || status === 'loading';
+    useEffect(() => {
+        dispatch(authCheck());
+        if (isAuthenticated) {
+            dispatch(all());
+            dispatch(recent());
+        }
     }, [isAuthenticated]);
 
-  return (
-    <>
-      <Background />
-      
-      {/* {modalOpen && <div className="absolute w-screen bg-black/30 h-screen top-0 left-0 z-25"></div>} */}
-      <div 
-        style={{ "--bg-accent": theme.accent, "--bg-accent-hover": theme.accentHover, "--bg-primary": theme.primary, "--fill-primary-sticker": theme.primarySticker }}
-        className='max-w-[1480px] h-screen mx-auto flex flex-col justify-center p-10 font-archivo relative'>
-        
-        {/* Pfp and ui tools */}
-        <div className={`w-full z-20 flex justify-between ${profile? "delay-400 translate-y-0 opacity-100" : "-translate-y-[200%] opacity-0 pointer-events-none" } transition-all transform-gpu ease-[cubic-bezier(0.34,1.80,0.54,1)] duration-400`}>
-          <TopSection />
-        </div>
+    return (
+        <>
+            <Background />
+            {isBooting && (
+                <div
+                    style={{ '--bg-accent': theme.accent }}
+                    className="absolute z-60 flex h-screen w-screen items-center justify-center bg-black/40 pb-4"
+                >
+                    <span className="loader h-full w-full"></span>
+                </div>
+            )}
+            {/* {modalOpen && <div className="absolute w-screen bg-black/30 h-screen top-0 left-0 z-25"></div>} */}
+            <div
+                style={{
+                    '--bg-accent': theme.accent,
+                    '--bg-accent-hover': theme.accentHover,
+                    '--bg-primary': theme.primary,
+                    '--fill-primary-sticker': theme.primarySticker,
+                }}
+                className="font-archivo relative mx-auto flex h-screen max-w-[1480px] flex-col justify-center p-10"
+            >
+                {/* Pfp and ui tools */}
+                <div
+                    className={`z-20 flex w-full justify-between ${profile ? 'translate-y-0 opacity-100 delay-400' : 'pointer-events-none -translate-y-[200%] opacity-0'} transform-gpu transition-all duration-400 ease-[cubic-bezier(0.34,1.80,0.54,1)]`}
+                >
+                    <TopSection />
+                </div>
 
-        {/* Main Section */}
-        <div className='flex justify-center w-full mt-12 h-full max-h-[520px]'>
+                {/* Main Section */}
+                <div className="mt-12 flex h-full max-h-[520px] w-full justify-center">
+                    {/* Sessions categorized by status */}
+                    <div
+                        inert={!profile}
+                        className={`h-full w-[220px] shrink-0 ${profile ? 'translate-x-0 opacity-100 delay-800' : 'pointer-events-none -translate-x-[400%] opacity-0'} transform-gpu transition-all duration-400 ease-[cubic-bezier(0.34,1.40,0.54,1)]`}
+                    >
+                        <SessionCategories setCreate={setCreate} />
+                    </div>
 
-          {/* Sessions categorized by status */}
-          <div
-            inert={!profile}
-            className={`w-[220px] h-full shrink-0 ${profile? "delay-800 translate-x-0 opacity-100" : "-translate-x-[400%] opacity-0 pointer-events-none" } transition-all transform-gpu ease-[cubic-bezier(0.34,1.40,0.54,1)] duration-400`}>
-            <SessionCategories setCreate={setCreate}/>
-          </div>
+                    {/* Main Window */}
+                    <div
+                        className={` ${profile ? 'w-full min-w-[600px]' : 'max-h-[530px] min-h-[525px] w-[450px] min-w-[450px] -translate-y-[20%]'} relative ml-12 transition-all duration-600`}
+                    >
+                        <div
+                            className={`relative flex h-full w-full flex-col bg-[var(--bg-primary)] shadow-[8px_8px_0px_3px_black] outline-[5px]`}
+                        >
+                            <div className="pointer-events-none absolute top-0 left-0 z-0 h-full w-full overflow-hidden">
+                                <svg
+                                    className={`absolute -bottom-40 -left-15 z-1 h-90 w-90 fill-[var(--fill-primary-sticker)]`}
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="-20 0 510 510"
+                                >
+                                    <path d="M450 210c-26.9 0-36.4-35.6-13.1-49a30 30 0 1 0-30-52c-23.3 13.5-49.3-12.6-36-35.9a30 30 0 0 0-52-30h.1c-13.4 23.3-49 13.8-49-13a30 30 0 1 0-60 0c0 26.8-35.6 36.3-49 13a30 30 0 1 0-52 30c13.5 23.3-12.6 49.4-35.9 36a30 30 0 0 0-30 52c23.3 13.3 13.8 48.9-13 48.9a30 30 0 1 0 0 60c26.8 0 36.3 35.6 13 49a30 30 0 1 0 30 52c23.3-13.5 49.4 12.6 36 35.9a30 30 0 0 0 52 30h-.1c13.4-23.3 49-13.8 49 13a30 30 0 1 0 60 0c0-26.8 35.6-36.3 49-13a30 30 0 1 0 52-30c-13.4-23.3 12.6-49.4 35.9-36a30 30 0 0 0 30-52c-23.3-13.3-13.8-48.9 13.1-48.9a30 30 0 1 0 0-60Z"></path>
+                                </svg>
+                                <svg
+                                    className={`absolute top-20 -right-20 z-1 h-60 w-60 fill-[var(--fill-primary-sticker)]`}
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 480 480"
+                                >
+                                    <path d="M120 120h120v120H120zM0 240h120v120H0zM120 360h120v120H120zM0 0h120v120H0zM360 120h120v120H360zM240 240h120v120H240zM360 360h120v120H360zM240 0h120v120H240z"></path>
+                                </svg>
+                            </div>
+                            <div className="relative h-full w-full">
+                                {/* View/Create View */}
+                                <div
+                                    className={`absolute inset-0 transition-all duration-500 ${profile ? 'pointer-events-auto scale-100 opacity-100 delay-300' : 'pointer-events-none scale-95 opacity-0'} `}
+                                >
+                                    <div></div>
+                                    {profile && <SessionDetails />}
+                                    {profile && <CreateSession />}
+                                </div>
 
-          {/* Main Window */}
-          <div className={` ${profile? "w-full min-w-[600px]" : "w-[450px] min-w-[450px] -translate-y-[20%] min-h-[525px] max-h-[530px]"} relative transition-all duration-600 ml-12`}>
-            <div className={` relative flex flex-col h-full w-full  bg-[var(--bg-primary)] outline-[5px] shadow-[8px_8px_0px_3px_black]`}>
-              <div className='top-0 left-0 pointer-events-none overflow-hidden absolute w-full h-full z-0'>
-                <svg className={`fill-[var(--fill-primary-sticker)] absolute -bottom-40 -left-15 h-90 w-90 z-1`} xmlns="http://www.w3.org/2000/svg" viewBox="-20 0 510 510"><path d="M450 210c-26.9 0-36.4-35.6-13.1-49a30 30 0 1 0-30-52c-23.3 13.5-49.3-12.6-36-35.9a30 30 0 0 0-52-30h.1c-13.4 23.3-49 13.8-49-13a30 30 0 1 0-60 0c0 26.8-35.6 36.3-49 13a30 30 0 1 0-52 30c13.5 23.3-12.6 49.4-35.9 36a30 30 0 0 0-30 52c23.3 13.3 13.8 48.9-13 48.9a30 30 0 1 0 0 60c26.8 0 36.3 35.6 13 49a30 30 0 1 0 30 52c23.3-13.5 49.4 12.6 36 35.9a30 30 0 0 0 52 30h-.1c13.4-23.3 49-13.8 49 13a30 30 0 1 0 60 0c0-26.8 35.6-36.3 49-13a30 30 0 1 0 52-30c-13.4-23.3 12.6-49.4 35.9-36a30 30 0 0 0 30-52c-23.3-13.3-13.8-48.9 13.1-48.9a30 30 0 1 0 0-60Z"></path></svg>
-                <svg className={`fill-[var(--fill-primary-sticker)] absolute top-20 -right-20 h-60 w-60 z-1`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 480"><path d="M120 120h120v120H120zM0 240h120v120H0zM120 360h120v120H120zM0 0h120v120H0zM360 120h120v120H360zM240 240h120v120H240zM360 360h120v120H360zM240 0h120v120H240z"></path></svg>
-              </div>
-              <div className="relative h-full w-full">
-                  {/* View/Create View */}
-                  <div className={`
-                    transition-all duration-500 absolute inset-0
-                    ${profile ? "delay-300 opacity-100 scale-100 pointer-events-auto" : "opacity-0 scale-95 pointer-events-none"}
-                  `}>
-                    <div></div>
-                    {profile && <SessionDetails />}
-                    {profile && <CreateSession />}
-                  </div>
+                                {/* Login View */}
+                                <div
+                                    className={`absolute inset-0 z-20 transition-all duration-500 ${!profile ? 'pointer-events-auto scale-100 opacity-100 delay-400' : 'pointer-events-none scale-90 opacity-0'} `}
+                                >
+                                    <LoginForm />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                  {/* Login View */}
-                  <div className={`
-                    transition-all duration-500 absolute inset-0 z-20
-                    ${!profile ? "opacity-100 scale-100 pointer-events-auto delay-400" : "opacity-0 scale-90 pointer-events-none"}
-                  `}> 
-
-                    <LoginForm />
-                  </div>
-              </div>
+                    {/* Previous Session Window */}
+                    <div
+                        className={` ${profile ? 'translate-x-0 opacity-100 delay-600' : 'pointer-events-none translate-y-[200%] opacity-0'} relative ml-12 h-full w-[260px] shrink-0 transform-gpu transition-all duration-400 ease-[cubic-bezier(0.34,1.40,0.54,1)]`}
+                    >
+                        <PreviousSessions />
+                    </div>
+                </div>
             </div>
-          </div>
-
-          {/* Previous Session Window */}
-          <div className={` ${profile? "delay-600 translate-x-0 opacity-100" : "translate-y-[200%] opacity-0 pointer-events-none" } relative transition-all transform-gpu ease-[cubic-bezier(0.34,1.40,0.54,1)] duration-400 ml-12 w-[260px] h-full shrink-0`}>
-              <PreviousSessions />
-          </div>
-        </div>
-      </div>
-    </>
-  )
+        </>
+    );
 }
 
-export default App
+export default App;
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,123 +1,155 @@
+import { useEffect, useState } from 'react';
+import { login, register } from '../state/user/userThunks';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { useEffect, useState } from "react";
-import { login, register } from "../state/user/userThunks";
-import { useDispatch, useSelector } from "react-redux";
-
-export default function LoginForm(){
-    const { profile, status, error, isAuthenticated } = useSelector( s => s.user)
-    const { theme } = useSelector( s => s.ui )
+export default function LoginForm() {
+    const { profile, status, error, isAuthenticated } = useSelector(
+        (s) => s.user
+    );
+    const { theme } = useSelector((s) => s.ui);
     const dispatch = useDispatch();
-    const [registering, setRegistering] = useState(false)
-    const isBooting = status === "idle" || status === "loading";
-    const [formData, setFormData] = useState({ name:"", email:"", password: "" })
-    
-    useEffect(()=>{
-        setFormData({ name:"", email:"", password: "" })
-    },[profile])
-    
-    function handleChange(e){
-        if(!registering && e.target.id === "name"){
-            return
-        }
-        setFormData(prev => ({...prev, [e.target.id]: e.target.value}))
-    }
-    
-    function handleSubmit(){
+    const [registering, setRegistering] = useState(false);
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+    });
 
-        if(registering){
-            if(formData['name'].length < 6) return;
-            dispatch(register(formData))
+    useEffect(() => {
+        setFormData({ name: '', email: '', password: '' });
+    }, [profile]);
+
+    function handleChange(e) {
+        if (!registering && e.target.id === 'name') {
             return;
         }
-        dispatch(login({email:formData["email"], password: formData["password"]}))
+        setFormData((prev) => ({ ...prev, [e.target.id]: e.target.value }));
     }
 
-    if(isBooting){
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (registering) {
+            if (formData['name'].length < 6) return;
+            dispatch(register(formData));
+            return;
+        }
+        dispatch(
+            login({ email: formData['email'], password: formData['password'] })
+        );
+    }
+
+    if (!isAuthenticated) {
         return (
-            <div
-                style={{ "--bg-accent": theme.accent }} 
-                className=" flex items-center justify-center pb-4 w-full h-full">
-                <span className="loader w-full h-full"></span>
-            </div>
-        )
-    }
-
-    if(!isAuthenticated){
-    return(
-        <div 
-            style={{ "--bg-accent": theme.accent, "--bg-accent-hover": theme.accentHover, "--fill-primary-sticker": theme.primarySticker }}
-            className="relative flex flex-col h-full transition-all duration-200">
-            <div  className="transition-all h-full transition-discrete">
-                <h1 className={`${registering?"text-4xl":"text-5xl"} px-4 py-2 border-b-4 mb-4 bg-[var(--fill-primary-sticker)]`}>{registering?"Create Account":"Log In"}</h1>
-
-                {error && <p className="text-center text-xl bg-black text-red-500 tracking-wide p-1">{error.message}</p>}
-                
-                <div className="fixed w-full h-fit overflow-hidden px-8 pb-10">
-                    <form id="authForm" onSubmit={handleSubmit} className={`${registering? "translate-y-0": "-translate-y-20"} ease-[cubic-bezier(0.34,1.70,0.54,1)] duration-200 transition-all`}>
-                        <span 
-                            inert={!registering}
-                            className={`flex flex-col mb-3 ${registering?"pointer-events-auto" : "pointer-events-none"}`}>
-                            <label className="text-2xl" htmlFor="name">Name</label>
-                            <input 
-                            className="bg-white/90 h-fit mt-1 px-2 py-1 outline-[5px] shadow-[8px_8px_0px_3px_black]"
-                            type="text"
-                            id="name"
-                            required={registering}
-                            value={formData["name"]}
-                            onChange={(e) => handleChange(e)} 
-                            />
-                        </span>
-
-                        <span className="flex flex-col mb-3">
-                            <label className="text-2xl" htmlFor="email">Email</label>
-                            <input 
-                            className="bg-white/90 h-fit mt-1 px-2 py-1 outline-[5px] shadow-[8px_8px_0px_3px_black]"
-                            type="text"
-                            id="email"
-                            required={true}
-                            value={formData["email"]}
-                            onChange={(e) => handleChange(e)} 
-                            />
-                        </span>
-                        
-                        <span className="flex flex-col">
-                            <label className="text-2xl" htmlFor="password">Password</label>
-                            <input 
-                            className="bg-white/90 h-fit mt-1 px-2 py-1 outline-[5px] shadow-[8px_8px_0px_3px_black]"
-                            type="password" 
-                            id="password"
-                            required={true}
-                            value={formData["password"]}
-                            onChange={(e) => handleChange(e)}
-                            />
-                        </span>
-                    </form>
-                </div>
-            </div>
-            
-            <div className="mt-17 mb-30 text-center z-10">
-                {registering?"Already have an account?": "Don't have an account?"} 
-                <span 
-                    onClick={() => setRegistering(prev => !prev)}
-                    className="ml-2 hover:cursor-pointer text-[var(--fill-primary-sticker)]">
-                    {registering?"Log In":"Register"}
-                </span>
-            </div>
-            
-            <div className="absolute bottom-5 -left-1/2 h-25 w-[800px] overflow-hidden">
-                <div className={` ${profile? "animate-slide-out" : "animate-slide-in"} group w-fit h-fit bottom-6 relative hover:cursor-pointer mt-12 ml-auto left-100 `}>
-                    <span className="block text-transparent text-5xl font-bold py-1 px-4 text-center w-[205px]">Log In</span>
-                    <button 
-                        form="authForm"
-                        className={`group-hover:bg-[var(--bg-accent-hover)] ml-auto mr-6 mb-2 group-hover:translate-y-1 group-hover:translate-x-1 group-hover:cursor-pointer group-hover:shadow-[6px_6px_0px_3px_black] 
-                                    transition-all duration-50 absolute top-0
-                                    text-5xl font-bold py-1 px-4 text-center w-[205px]
-                                    outline-[5px] outline-black shadow-[10px_10px_0px_3px_black] bg-[var(--bg-accent)]`}
+            <>
+                <div
+                    style={{
+                        '--bg-accent': theme.accent,
+                        '--bg-accent-hover': theme.accentHover,
+                        '--fill-primary-sticker': theme.primarySticker,
+                    }}
+                    className="relative flex h-full flex-col transition-all duration-200"
+                >
+                    <div className="h-full transition-all transition-discrete">
+                        <h1
+                            className={`${registering ? 'text-4xl' : 'text-5xl'} mb-4 border-b-4 bg-[var(--fill-primary-sticker)] px-4 py-2`}
                         >
-                        {registering?"Create":"Log In"}
-                    </button>
+                            {registering ? 'Create Account' : 'Log In'}
+                        </h1>
+
+                        {error && (
+                            <p className="bg-black p-1 text-center text-xl tracking-wide text-red-500">
+                                {error.message}
+                            </p>
+                        )}
+
+                        <div className="fixed h-fit w-full overflow-hidden px-8 pb-10">
+                            <form
+                                id="authForm"
+                                onSubmit={handleSubmit}
+                                className={`${registering ? 'translate-y-0' : '-translate-y-20'} transition-all duration-200 ease-[cubic-bezier(0.34,1.70,0.54,1)]`}
+                            >
+                                <span
+                                    inert={!registering}
+                                    className={`mb-3 flex flex-col ${registering ? 'pointer-events-auto' : 'pointer-events-none'}`}
+                                >
+                                    <label className="text-2xl" htmlFor="name">
+                                        Name
+                                    </label>
+                                    <input
+                                        className="mt-1 h-fit bg-white/90 px-2 py-1 shadow-[8px_8px_0px_3px_black] outline-[5px]"
+                                        type="text"
+                                        id="name"
+                                        required={registering}
+                                        value={formData['name']}
+                                        onChange={(e) => handleChange(e)}
+                                    />
+                                </span>
+
+                                <span className="mb-3 flex flex-col">
+                                    <label className="text-2xl" htmlFor="email">
+                                        Email
+                                    </label>
+                                    <input
+                                        className="mt-1 h-fit bg-white/90 px-2 py-1 shadow-[8px_8px_0px_3px_black] outline-[5px]"
+                                        type="text"
+                                        id="email"
+                                        required={true}
+                                        value={formData['email']}
+                                        onChange={(e) => handleChange(e)}
+                                    />
+                                </span>
+
+                                <span className="flex flex-col">
+                                    <label
+                                        className="text-2xl"
+                                        htmlFor="password"
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        className="mt-1 h-fit bg-white/90 px-2 py-1 shadow-[8px_8px_0px_3px_black] outline-[5px]"
+                                        type="password"
+                                        id="password"
+                                        required={true}
+                                        value={formData['password']}
+                                        onChange={(e) => handleChange(e)}
+                                    />
+                                </span>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="z-10 mt-17 mb-30 text-center">
+                        {registering
+                            ? 'Already have an account?'
+                            : "Don't have an account?"}
+                        <span
+                            onClick={() => setRegistering((prev) => !prev)}
+                            className="ml-2 text-[var(--fill-primary-sticker)] hover:cursor-pointer"
+                        >
+                            {registering ? 'Log In' : 'Register'}
+                        </span>
+                    </div>
+
+                    <div className="absolute bottom-5 -left-1/2 h-25 w-[800px] overflow-hidden">
+                        <div
+                            className={` ${profile ? 'animate-slide-out' : 'animate-slide-in'} group relative bottom-6 left-100 mt-12 ml-auto h-fit w-fit hover:cursor-pointer`}
+                        >
+                            <span className="block w-[205px] px-4 py-1 text-center text-5xl font-bold text-transparent">
+                                Log In
+                            </span>
+                            <button
+                                type="submit"
+                                form="authForm"
+                                disabled={status === 'loading'}
+                                className={`absolute top-0 mr-6 mb-2 ml-auto w-[205px] bg-[var(--bg-accent)] px-4 py-1 text-center text-5xl font-bold shadow-[10px_10px_0px_3px_black] outline-[5px] outline-black transition-all duration-50 group-hover:translate-x-1 group-hover:translate-y-1 group-hover:cursor-pointer group-hover:bg-[var(--bg-accent-hover)] group-hover:shadow-[6px_6px_0px_3px_black]`}
+                            >
+                                {registering ? 'Create' : 'Log In'}
+                            </button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    )
-}}
+            </>
+        );
+    }
+}
